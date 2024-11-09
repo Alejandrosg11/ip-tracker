0b1de53da78670b8eaa5fd2d4b027537a9d4f4d0
Alex Santamaria <alejandrosg11@gmail.com>
2024-11-09 11:55:22 -0600

    validadores

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,15 @@ export default function Home() {
   const [lat, setLat] = useState(19.449252);
   const [lng, setLng] = useState(-99.1638502);
 
+  const isValidIP = (value: string) => {
+    const ipRegex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    return ipRegex.test(value);
+  };
+  
+  const isValidDomain = (value: string) => {
+    const domainRegex = /^(?!:\/\/)([a-zA-Z0-9-_]+\.)+[a-zA-Z]{2,11}?$/;
+    return domainRegex.test(value);
+  };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -18,8 +27,17 @@ export default function Home() {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const apiKey = "at_RxhwL1SzifSFiMG1r1c8Q4hagsfmU";
-    const url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${inputValue}`;
+    const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+    let url = `https://geo.ipify.org/api/v1?apiKey=${apiKey}`;
+
+    if (isValidIP(inputValue)) {
+      url += `&ipAddress=${inputValue}`;
+    } else if (isValidDomain(inputValue)) {
+      url += `&domain=${inputValue}`;
+    } else {
+      console.error("Invalid input");
+      return;
+    }
 
     try {
       const response = await fetch(url);
